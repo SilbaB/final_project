a3a41d1e712b5fe0d9edea921b49464dc6194f7a
SilbaB <murkomenbasil@gmail.com>
2024-09-05 15:01:33 +0300

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,14 +17,14 @@ cp = st.selectbox("cp", [1, 2, 3, 4])
 trestbps = st.number_input("trestbps", min_value=94, max_value=200, value=100, step=1)
 chol = st.number_input("chol", min_value=126, max_value=564, value=300, step=1)
 fbs= st.selectbox("fbs", [0, 1])
-# restecg= st.selectbox("restecg", [0, 1])
-# thalach= st.number_input("thalach", min_value=71, max_value=202, value=130, step=1)
+restecg= st.selectbox("restecg", [0, 1])
+thalach= st.number_input("thalach", min_value=71, max_value=202, value=130, step=1)
 exang= st.selectbox("exang", [0, 1])
 oldpeak= st.selectbox("oldpeak", [0, 1,2,3,4,5,6])
 slope= st.selectbox("fbs", [0, 1, 2, 3])
 ca= st.selectbox("ca", [0, 1, 2, 3])
 thal= st.selectbox("thal", [3, 4, 5, 6, 7])
-present= st.selectbox("present", [0, 1])
+# present= st.selectbox("present", [0, 1])
 
 if st.button("Predict"):
     # Process input values
@@ -36,14 +36,14 @@ if st.button("Predict"):
             "trestbps": [trestbps],
             "chol": [chol],
             "fbs": [fbs],
-            # "restecg": [restecg],
-            # "thalach": [thalach],
+            "restecg": [restecg],
+            "thalach": [thalach],
             "exang": [exang],
             "oldpeak": [oldpeak],
             "slope": [slope],
             "ca": [ca],
             "thal": [thal],
-            "present": [present],
+            # "present": [present],
 
           
         }
